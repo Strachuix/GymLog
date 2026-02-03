bf4d165b36b685f86b2a1d7b36de735b047b7e3f
Strachuix <stanislawpokropek@gmail.com>
2026-02-03 22:51:10 +0100

    feat: Implement profile page and update navigation links to point to profile

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const VERSION = '1.1.4';
+const VERSION = '1.2.0';
 const CACHE_NAME = `gymlog-v${VERSION}`;
 const urlsToCache = [
   '/',
   '/index.html',
   '/stats.html',
   '/history.html',
+  '/profile.html',
   '/app.js',
   '/install.js',
   '/manifest.json',
